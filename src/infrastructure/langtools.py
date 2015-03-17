67c3ebe1093d866aa5bde4433359f956ece045a1
James Townley <software@peachyprinter.com>
2015-03-17 13:43:55 -0600

    Language changing version simplistic

--- src/infrastructure/langtools.py
+++ src/infrastructure/langtools.py
@@ -1,4 +1,3 @@
-import os
 from os.path import join, dirname
 import gettext
 
--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
 import kivy
+from kivy.app import App
+from kivy.uix.settings import SettingsWithSidebar
+from kivy.properties import StringProperty
+from infrastructure.langtools import _
+
 kivy.require('1.8.0')
 
-from kivy.app import App
 
 class PeachyPrinter(App):
-    pass
+    lang = StringProperty('en_GB')
+
+    def build(self):
+        self.settings_cls = SettingsWithSidebar
+
+    def build_settings(self, settings):
+        self.settings = settings
+
+    def on_lang(self, instance, lang):
+        _.switch_lang(lang)
+        if hasattr(self, 'settings'):
+            self.settings.interface.menu.close_button.text = _("Close")
 
 if __name__ == '__main__':
     PeachyPrinter().run()
