0f6a73fe356530870ea8043a22a6b1d9f1011d63
James Townley <software@peachyprinter.com>
2015-03-26 15:00:58 -0600

    Performing dance to make to confirms load form peachy api

--- src/infrastructure/setting_mapper.py
+++ src/infrastructure/setting_mapper.py
@@ -23,54 +23,86 @@ class SettingsMapper(object):
                 {
                     'type': 'string',
                     'section': 'Info',
-                    'desc': _('info.version_number DESCRIPTION'),
-                    'title': _('info.version_number TITLE'),
-                    'key': 'info.version_number',
+                    'desc': _('info_version_number DESCRIPTION'),
+                    'title': _('info_version_number TITLE'),
+                    'key': 'info_version_number',
                     'disabled': True
                 },
                 {
                     'type': 'string',
                     'section': 'Info',
-                    'desc': _('info.serial_number DESCRIPTION'),
-                    'title': _('info.serial_number TITLE'),
-                    'key': 'info.serial_number',
+                    'desc': _('info_serial_number DESCRIPTION'),
+                    'title': _('info_serial_number TITLE'),
+                    'key': 'info_serial_number',
                     'disabled': True
                 },
                 {
                     'type': 'string',
                     'section': 'Info',
-                    'desc': _('info.hardware_version_number DESCRIPTION'),
-                    'title': _('info.hardware_version_number TITLE'),
-                    'key': 'info.hardware_version_number',
+                    'desc': _('info_hardware_version_number DESCRIPTION'),
+                    'title': _('info_hardware_version_number TITLE'),
+                    'key': 'info_hardware_version_number',
                     'disabled': True
                 },
                 {
                     'type': 'string',
                     'section': 'Info',
-                    'desc': _('info.firmwware_version_number DESCRIPTION'),
-                    'title': _('info.firmwware_version_number TITLE'),
-                    'key': 'info.firmwware_version_number',
+                    'desc': _('info_firmware_version_number DESCRIPTION'),
+                    'title': _('info_firmware_version_number TITLE'),
+                    'key': 'info_firmware_version_number',
                     'disabled': True
                 },
-               ]
-
-    @property
-    def config_options(self):
-        return [
+# ----------- BEGIN Options --------------------
                 {
                     'type': 'bool',
                     'section': 'Options',
-                    'key': 'options.use_sublayers',
-                    'title': _('options.use_sublayers TITLE'),
-                    'desc': _('options.use_sublayers DESCRIPTION'),
+                    'key': 'options_use_sublayers',
+                    'title': _('options_use_sublayers TITLE'),
+                    'desc': _('options_use_sublayers DESCRIPTION'),
                     'values': [True, False]
                 },
                 {
                     'type': 'numeric',
                     'section': 'Options',
-                    'key': 'options.sublayer_height_mm',
-                    'title': _('options.sublayer_height_mm TITLE'),
-                    'desc': _('options.sublayer_height_mm DESCRIPTION'),
+                    'key': 'options_sublayer_height_mm',
+                    'title': _('options_sublayer_height_mm TITLE'),
+                    'desc': _('options_sublayer_height_mm DESCRIPTION'),
+                    'value_range': [0, None],
+                    'ok_button_text': _("Ok"),
+                    'cancel_button_text': _("Cancel")
+                },
+                {
+                    'type': 'bool',
+                    'values': [True, False],
+                    'section': 'Options',
+                    'key': 'options_use_shufflelayers',
+                    'title': _('options_use_shufflelayers TITLE'),
+                    'desc': _('options_use_shufflelayers DESCRIPTION'),
+                },
+                {
+                    'type': 'numeric',
+                    'section': 'Options',
+                    'key': 'options_shuffle_layers_amount',
+                    'title': _('options_shuffle_layers_amount TITLE'),
+                    'desc': _('options_shuffle_layers_amount DESCRIPTION'),
+                    'value_range': [0, None],
+                    'ok_button_text': _("Ok"),
+                    'cancel_button_text': _("Cancel")
+                },
+                {
+                    'type': 'bool',
+                    'values': [True, False],
+                    'section': 'Options',
+                    'key': 'options_use_overlap',
+                    'title': _('options_use_overlap TITLE'),
+                    'desc': _('options_use_overlap DESCRIPTION'),
+                },
+                {
+                    'type': 'numeric',
+                    'section': 'Options',
+                    'key': 'options_overlap_amount_mm',
+                    'title': _('options_overlap_amount_mm TITLE'),
+                    'desc': _('options_overlap_amount_mm DESCRIPTION'),
                     'value_range': [0, None],
                     'ok_button_text': _("Ok"),
                     'cancel_button_text': _("Cancel")
@@ -78,71 +110,84 @@ class SettingsMapper(object):
                 {
                     'type': 'numeric',
                     'section': 'Options',
-                    'key': 'options.laser_thickness_mm',
-                    'title': _('options.laser_thickness_mm TITLE'),
-                    'desc': _('options.laser_thickness_mm DESCRIPTION'),
+                    'key': 'options_laser_thickness_mm',
+                    'title': _('options_laser_thickness_mm TITLE'),
+                    'desc': _('options_laser_thickness_mm DESCRIPTION'),
                     'value_range': [0, None]
                 },
                 {
                     'type': 'numeric',
                     'section': 'Options',
-                    'key': 'options.scaling_factor',
-                    'title': _('options.scaling_factor TITLE'),
-                    'desc': _('options.scaling_factor DESCRIPTION'),
+                    'key': 'options_scaling_factor',
+                    'title': _('options_scaling_factor TITLE'),
+                    'desc': _('options_scaling_factor DESCRIPTION'),
                     'value_range': [0, None],
                     'ok_button_text': _("Ok"),
                     'cancel_button_text': _("Cancel")
                 },
-                {
-                    'type': 'numeric',
-                    'section': 'Options',
-                    'key': 'options.overlap_amount_mm',
-                    'title': _('options.overlap_amount_mm TITLE'),
-                    'desc': _('options.overlap_amount_mm DESCRIPTION'),
-                    'value_range': [0, None],
-                    'ok_button_text': _("Ok"),
-                    'cancel_button_text': _("Cancel")
-                },
-                {
-                    'type': 'bool',
-                    'values': [True, False],
-                    'section': 'Options',
-                    'key': 'options.use_shufflelayers',
-                    'title': _('options.use_shufflelayers TITLE'),
-                    'desc': _('options.use_shufflelayers DESCRIPTION'),
-                },
-                {
-                    'type': 'bool',
-                    'values': [True, False],
-                    'section': 'Options',
-                    'key': 'options.use_overlap',
-                    'title': _('options.use_overlap TITLE'),
-                    'desc': _('options.use_overlap DESCRIPTION'),
-                },
-                {
-                    'type': 'numeric',
-                    'section': 'Options',
-                    'key': 'options.print_queue_delay',
-                    'title': _('options.print_queue_delay TITLE'),
-                    'desc': _('options.print_queue_delay DESCRIPTION'),
-                    'value_range': [0, None],
-                    'ok_button_text': _("Ok"),
-                    'cancel_button_text': _("Cancel")
-                },
-                {
-                    'type': 'numeric',
-                    'section': 'Options',
-                    'key': 'options.pre_layer_delay',
-                    'title': _('options.pre_layer_delay TITLE'),
-                    'desc': _('options.pre_layer_delay DESCRIPTION'),
-                    'value_range': [0, None],
-                    'ok_button_text': _("Ok"),
-                    'cancel_button_text': _("Cancel")
-                },]
 
-    @property
-    def config_email(self):
-        return [
+                {
+                    'type': 'numeric',
+                    'section': 'Options',
+                    'key': 'options_max_lead_distance_mm',
+                    'title': _('options_max_lead_distance_mm TITLE'),
+                    'desc': _('options_max_lead_distance_mm DESCRIPTION'),
+                    'value_range': [0, None],
+                    'ok_button_text': _("Ok"),
+                    'cancel_button_text': _("Cancel")
+                },
+                {
+                    'type': 'numeric',
+                    'section': 'Options',
+                    'key': 'options_pre_layer_delay',
+                    'title': _('options_pre_layer_delay TITLE'),
+                    'desc': _('options_pre_layer_delay DESCRIPTION'),
+                    'value_range': [0, None],
+                    'ok_button_text': _("Ok"),
+                    'cancel_button_text': _("Cancel")
+                },
+                {
+                    'type': 'numeric',
+                    'section': 'Options',
+                    'key': 'options_post_fire_delay',
+                    'title': _('options_post_fire_delay TITLE'),
+                    'desc': _('options_post_fire_delay DESCRIPTION'),
+                    'value_range': [0, None],
+                    'ok_button_text': _("Ok"),
+                    'cancel_button_text': _("Cancel")
+                },
+                                {
+                    'type': 'numeric',
+                    'section': 'Options',
+                    'key': 'options_slew_delay',
+                    'title': _('options_slew_delay TITLE'),
+                    'desc': _('options_slew_delay DESCRIPTION'),
+                    'value_range': [0, None],
+                    'ok_button_text': _("Ok"),
+                    'cancel_button_text': _("Cancel")
+                },
+                {
+                    'type': 'numeric',
+                    'section': 'Options',
+                    'key': 'options_wait_after_move_milliseconds',
+                    'title': _('options_wait_after_move_milliseconds TITLE'),
+                    'desc': _('options_wait_after_move_milliseconds DESCRIPTION'),
+                    'value_range': [0, None],
+                    'ok_button_text': _("Ok"),
+                    'cancel_button_text': _("Cancel")
+                },
+
+                {
+                    'type': 'numeric',
+                    'section': 'Options',
+                    'key': 'options_print_queue_delay',
+                    'title': _('options_print_queue_delay TITLE'),
+                    'desc': _('options_print_queue_delay DESCRIPTION'),
+                    'value_range': [0, None],
+                    'ok_button_text': _("Ok"),
+                    'cancel_button_text': _("Cancel")
+                },
+# ----------- BEGIN EMAIL --------------------
                 {
                     'type': 'bool',
                     'section': 'Email',
@@ -207,19 +252,16 @@ class SettingsMapper(object):
                     'cancel_button_text': _("Cancel")
                 },]
 
-    @property
-    def section_map(self):
-        return {
-            _('Info'): self.config_info,
-            _('Options'): self.config_options,
-            _('Email'): self.config_email,
-        }
-
     def refresh_settings(self, settings, config):
+        Logger.info("Starting")
         settings.register_type('string', SettingString)
         settings.register_type('bool', SettingBoolean)
         settings.register_type('numeric', SettingNumeric)
-        for (section, data) in self.section_map.items():
+
+        sections = set([item['section'] for item in self.config_info])
+        for section in sections:
+            Logger.info("SECTION ADDED: %s" % section)
+            data = [cfg for cfg in self.config_info if cfg['section'] == section]
             settings.add_json_panel(section, config, data=json.dumps(data))
 
     def set_defaults(self, config):
@@ -239,40 +281,18 @@ class SettingsMapper(object):
     def update_setting(self, section, key, value):
         Logger.info(u"Setting changed  %s, %s -> %s" % (section, key, value))
         entry_type = [entry['type'] for entry in self.section_map[section] if entry['key'] == key][0]
-        if '.' in key:
-            setter = key.split('.')[1]
-        else:
-            setter = key
-        getattr(self.configuration_api, 'set_' + setter)(self._convert(entry_type, value))
+        getattr(self.configuration_api, 'set_' + key)(self._convert(entry_type, value))
         self.configuration_api.save()
 
     def load_config(self, config):
         Logger.info("Loading Configs")
         self.configuration_api.load_printer(self.configuration_api.get_available_printers()[0])
-        info_items = {
-            'info.version_number': '%s, build %s' % (version, revision),
-            'info.serial_number': 'Not Yet Determined',
-            'info.hardware_version_number': 'Not Yet Determined',
-            'info.firmwware_version_number': 'Not Yet Determined',
-            }
 
-        config_items = {}
-        for item in self.config_options:
+        for item in self.config_info:
             key = item['key']
-            getter = 'get_' + key.split('.')[1]
-            config_items[key] = getattr(self.configuration_api, getter)()
-
-        email_items = {}
-        for item in self.config_email:
-            key = item['key']
+            section = item['section']
             getter = 'get_' + key
-            email_items[key] = getattr(self.configuration_api, getter)()
-
-        self.setall(config, 'Info', info_items)
-        self.setall(config, 'Options', config_items)
-        self.setall(config, 'Email', email_items)
-
-    def setall(self, config, section, items):
-        config.add_section(section)
-        for (key, value) in items.items():
+            value = getattr(self.configuration_api, getter)()
+            if not config.has_section(section):
+                config.add_section(section)
             config.set(section, key, value)
