bad85fbd6b54bf2074320517f42a0b464d1f86bc
James Townley <software@peachyprinter.com>
2015-04-07 17:05:37 -0600

    Added more to calibration screens

--- src/ui/calibrate_ui.py
+++ src/ui/calibrate_ui.py
@@ -10,9 +10,13 @@ Builder.load_file('ui/calibrate_ui.kv')
 
 class CalibrateUI(Screen):
     calibration_point = ListProperty([0, 0])
+    example_point = ListProperty([0, 0])
+    example_dot = ListProperty([100, 100])
 
     def __init__(self, **kwargs):
         super(CalibrateUI, self).__init__(**kwargs)
+        self.bind(example_point=self.on_example_point)
+
 
     def on_motion(self, etype, motionevent, mouse_pos):
         (x, y) = self.ids.top_calibration_grid.pos
@@ -28,9 +32,30 @@ class CalibrateUI(Screen):
             self.calibration_point = mouse_pos.pos
             # CALL TO API HERE
 
+    def on_upper_left(self):
+        self.example_point = [0, 1]
+
+    def on_upper_right(self):
+        self.example_point = [1, 1]
+
+    def on_lower_right(self):
+        self.example_point = [1, 0]
+
+    def on_lower_left(self):
+        self.example_point = [0, 0]
 
     def on_pre_enter(self):
         Window.bind(on_motion=self.on_motion)
+
+    def on_example_point(self, *args):
+        image_size =  min(self.ids.example_grid.size)
+        pos_x = self.ids.example_grid.x + (self.ids.example_grid.width - image_size) / 2
+        pos_y = self.ids.example_grid.y + (self.ids.example_grid.height - image_size) / 2
+        self.example_dot = [
+            pos_x - 4 + (self.example_point[0] * image_size),
+            pos_y - 4 + (self.example_point[1] * image_size),
+            ]
+        Logger.info("Called %s,%s" % (self.example_dot[0],self.example_dot[1]))
 
     def on_pre_leave(self):
         Window.unbind(on_motion=self.on_motion)
