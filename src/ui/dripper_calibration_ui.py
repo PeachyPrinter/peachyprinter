a387d4604c0892e1b4f2081fdfb32937921ecbfd
james townley <james@townley.ca>
2015-04-15 09:44:31 -0600

    Added dripper setup and calculator

--- src/ui/dripper_calibration_ui.py
+++ src/ui/dripper_calibration_ui.py
@@ -39,7 +39,7 @@ class DripperCalibrationUI(Screen):
             self.ids.dripper_setup.add_widget(EmulatedDripSetup(self.configuration_api))
         elif value == 'photo':
             self.ids.dripper_setup.add_widget(PhotoDripSetup(self.configuration_api))
-        elif value =='microcontroller':
+        elif value == 'microcontroller':
             self.ids.dripper_setup.add_widget(MicrocontrollerDripSetup(self.configuration_api))
 
         self.configuration_api.set_dripper_type(value)
@@ -51,18 +51,12 @@ class DripperCalibrationUI(Screen):
         self.ids.dripper_type_selector.key = dripper_type
         self.dripper_type_changed(None, dripper_type)
 
-
     def on_pre_leave(self):
         self.is_active = False
+        self.ids.dripper_setup.clear_widgets()
         if self.configuration_api:
             self.configuration_api.stop_counting_drips()
         self.configuration_api = None
-
-
-# set_dripper_drips_per_mm
-# set_dripper_type
-# set_dripper_emulated_drips_per_second
-# set_dripper_photo_zaxis_delay
 
 
 class EmulatedDripSetup(BoxLayout):
@@ -98,22 +92,49 @@ class PhotoDripSetup(BoxLayout):
         Logger.info('photo zaxis delay set to %s' % value)
         self.configuration_api.set_dripper_photo_zaxis_delay(value)
 
+
 class MicrocontrollerDripSetup(BoxLayout):
-    total_height = NumericProperty(100)
+    total_height = NumericProperty(100.0)
     drips_per_mm = NumericProperty(1.0)
+    current_drips_per_mm = NumericProperty(0.0)
     drips = NumericProperty(0.0)
+    average_drips = NumericProperty(0.0)
 
     def __init__(self, api, **kwargs):
         self.is_active = False
         self.configuration_api = api
         super(MicrocontrollerDripSetup, self).__init__(**kwargs)
+        Logger.info("Starting up dripper")
+        self.ids.ui_drips_per_mm.text = '%.2f' % self.configuration_api.get_dripper_drips_per_mm()
+        self.configuration_api.start_counting_drips(self.drip_call_back)
+
+    def drip_call_back(self, drips, current_z_location_mm, average_drips, drip_history):
+        self.drips = drips
+        self.average_drips = average_drips
+
+    def on_parent(self, instance, value):
+        if value is None:
+            Logger.info("Shutting down dripper")
+            self.configuration_api.stop_counting_drips()
 
     def update_total_height(self):
         self.total_height = self.ids.end_height.text - self.ids.start_height.text
         self.update_drips_per_mm()
 
     def update_drips_per_mm(self):
-        self.drips_per_mm = self.drips / self.total_height
+        self.current_drips_per_mm = self.drips / self.total_height
 
     def on_drips(self, instance, value):
         self.update_drips_per_mm()
+
+    def on_drips_per_mm(self, instance, value):
+        Logger.info("Useing the drips per mm amount %.2f" % value)
+        self.ids.ui_drips_per_mm.text = '%.2f' % value
+        self.configuration_api.set_dripper_drips_per_mm(value)
+
+    def use_current(self):
+        Logger.info("Useing current drips")
+        self.drips_per_mm = self.current_drips_per_mm
+
+    def reset_drip_count(self):
+        self.configuration_api.reset_drips()
