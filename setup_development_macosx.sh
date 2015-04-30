2e29fb6ca36493089e2c78721c7d7be71c3176e3
james townley <james@townley.ca>
2015-04-30 09:39:33 -0600

    Making progress on adding dylib's for usblib1 to kivy

--- setup_development_macosx.sh
+++ setup_development_macosx.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 echo "----------Creating symlinks to kivy venv------------"
 ln -s /Applications/Kivy.app/Contents/Resources/venv venv
+
 echo "----------Activating kivy venv------------"
-source /Applications/Kivy.app/Contents/Resources/venv/bin/activate
+KIVY_APP_PATH="/Applications/Kivy.app/Contents/Resources";
+
+# activate the virtualenv
+source ${KIVY_APP_PATH}/venv/bin/activate
+
+# setup the environment to not mess with the system
+export PYTHONPATH="${KIVY_APP_PATH}/kivy:${PYTHONPATH}"
+export DYLD_FALLBACK_LIBRARY_PATH="${KIVY_APP_PATH}/lib,$DYLD_FALLBACK_LIBRARY_PATH"
+export LD_PRELOAD_PATH="${KIVY_APP_PATH}/lib"
+export GST_REGISTRY="${KIVY_APP_PATH}/gst.registry"
+export GST_PLUGIN_SCANNER="${KIVY_APP_PATH}/gst-plugin-scanner"
+export GTK_PATH="${KIVY_APP_PATH}/../Frameworks/GStreamer.framework/Versions/Current"
+export GST_PLUGIN_SYSTEM_PATH="${KIVY_APP_PATH}/../Frameworks/GStreamer.framework/Versions/Current/lib/gstreamer-1.0"
+export GIO_EXTRA_MODULES="${KIVY_APP_PATH}/../Frameworks/GStreamer.framework/Versions/Current/lib/gio/modules"
+export KIVY_HOME="${KIVY_APP_PATH}/.kivy"
 
 echo "--------Setting up pyinstaller----"
 pip install -U pyinstaller
@@ -17,4 +32,6 @@ python -m compileall venv/lib/python2.7/site-packages/google/
 echo ""
 echo "-----------------------------------"
 echo "Enviroment setup complete and seemingly successful."
-echo "You can start the enviroment with the command\"source venv/bin/activate\""
+
+export DYLD_FALLBACK_LIBRARY_PATH="${SCRIPT_PATH}/lib"
+export DYLD_FALLBACK_LIBRARY_PATH="${SCRIPT_PATH}/lib,$DYLD_FALLBACK_LIBRARY_PATH"
