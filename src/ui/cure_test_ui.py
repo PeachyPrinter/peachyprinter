ccfd57fc15a945332b55764a55083cee5fb8b718
James Townley <software@peachyprinter.com>
2015-06-11 10:06:24 -0600

    Starting to glus the pieces together

--- src/ui/cure_test_ui.py
+++ src/ui/cure_test_ui.py
@@ -49,7 +49,7 @@ class CureTestUI(Screen):
         self.loaded = False
         super(CureTestUI, self).__init__(**kwargs)
         self.base_speed = BaseSpeed()
-        self.ids.total_height.value = max(self.base + 10, self.test_height)
+        self.ids.total_height_id.value = max(self.base + 10, self.test_height)
 
     def show_base_speed(self, value):
         if value is True:
@@ -124,9 +124,9 @@ class CureTestUI(Screen):
         self.touched(touch)
 
     def touched(self, touch):
-        coords = self.ids.test_height_image.to_widget(*touch.pos)
-        if self.ids.test_height_image.collide_point(*coords):
-            self.ids.best_height_image.y = coords[1]
+        coords = self.ids.test_height_image_id.to_widget(*touch.pos)
+        if self.ids.test_height_image_id.collide_point(*coords):
+            self.ids.best_height_image_id.y = coords[1]
             return True
 
 
@@ -141,15 +141,15 @@ class CureTestUI(Screen):
 
     def on_pre_enter(self):
         self.configuration_api = self.api.get_configuration_api()
-        # self.ids.base_id.text = '%.2f' % self.configuration_api.get_cure_rate_base_height()
-        # self.ids.height_id.text = '%.2f' % self.configuration_api.get_cure_rate_total_height()
-        # self.ids.start_speed_id.text = '%.2f' % self.configuration_api.get_cure_rate_start_speed()
-        # self.ids.stop_speed_id.text = '%.2f' % self.configuration_api.get_cure_rate_finish_speed()
-        # self.ids.use_draw_speed_id.active = self.configuration_api.get_cure_rate_use_draw_speed()
-        # self.ids.draw_speed_id.text = '%.2f' % self.configuration_api.get_cure_rate_draw_speed()
-        # self.ids.move_speed_id.text = '%.2f' % self.configuration_api.get_cure_rate_move_speed()
-        # self.ids.override_laser_power_id.active = self.configuration_api.get_cure_rate_override_laser_power()
-        # self.ids.override_laser_power_amount_id.text = '%.2f' % self.configuration_api.get_cure_rate_override_laser_power_amount()
+        self.ids.base_height_id.value = self.configuration_api.get_cure_rate_base_height()
+        self.ids.total_height_id.value = self.configuration_api.get_cure_rate_total_height()
+        self.ids.minimum_speed_id.value = self.configuration_api.get_cure_rate_start_speed()
+        self.ids.maximum_speed_id.value = self.configuration_api.get_cure_rate_finish_speed()
+        self.ids.use_draw_speed_id.active = self.configuration_api.get_cure_rate_use_draw_speed()
+        self.ids.draw_speed_id.text = '%.2f' % self.configuration_api.get_cure_rate_draw_speed()
+        self.ids.move_speed_id.text = '%.2f' % self.configuration_api.get_cure_rate_move_speed()
+        self.ids.override_laser_power_id.active = self.configuration_api.get_cure_rate_override_laser_power()
+        self.ids.override_laser_power_amount_id.text = '%.2f' % self.configuration_api.get_cure_rate_override_laser_power_amount()
 
     def on_enter(self):
         self.loaded = True
