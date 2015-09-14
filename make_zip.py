5732a6d0c142ad76243a8efdabbabdb59dcf99ce
James Townley <software@peachyprinter.com>
2015-09-14 09:39:19 -0600

    Adding zipping

--- make_zip.py
+++ make_zip.py
@@ -7,10 +7,13 @@ import zipfile
 def zip_dir(dir_to_zip, zip_file_handle):
     for root, dirs, files in os.walk(dir_to_zip):
         for current_file in files:
-            zip_file_handle.write(os.path.join(root, file))
+            zip_file_handle.write(os.path.join(root, current_file))
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         Exception('Must provide directory and output file name')
-    with zipfile.ZipFile(sys.argv[2], 'w') as zip_file_handle:
-        zip_dir(sys.argv[1], zip_file_handle)
+    out_file = sys.argv[2]
+    in_folder = sys.argv[1]
+    print("Zipping from {} into {}".format(in_folder, out_file))
+    with zipfile.ZipFile(out_file, 'w') as zip_file_handle:
+        zip_dir(in_folder, zip_file_handle)
