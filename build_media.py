3bcdfe6b4cd38ad8e4818b1af16b1603a0804587
James Townley <software@peachyprinter.com>
2016-03-04 11:44:18 -0600

    Fixing up media build to make more sense and package files better

--- build_media.py
+++ build_media.py
@@ -5,9 +5,11 @@ import zipfile
 
 def zip_dir(dir_to_zip, zip_file_name):
     with zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED) as zip_file_handle:
-        for root, dirs, files in os.walk(dir_to_zip):
-            for current_file in files:
-                zip_file_handle.write(os.path.join(root, current_file))
+        files = os.listdir(dir_to_zip)
+        files.sort()
+        for current_file in files:
+            print ("Processing: {}".format(current_file))
+            zip_file_handle.write(os.path.join(dir_to_zip, current_file), current_file)
 
 if __name__ == '__main__':
     current_file_path = os.path.dirname(os.path.realpath(__file__))
@@ -18,6 +20,6 @@ if __name__ == '__main__':
     for folder in os.listdir(media_path):
         source_path = os.path.join(media_path, folder)
         destination_file = os.path.join(output_path, folder + '.zip')
-        print ("Zipping {} into {}".format(source_path,destination_file))
-        zip_dir(source_path,destination_file)
+        print ("Zipping {} into {}".format(source_path, destination_file))
+        zip_dir(source_path, destination_file)
         print ("Complete writing {}".format(destination_file))
